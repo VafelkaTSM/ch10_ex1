4efb7cb04e968b173b1c76192dcdd019b57d6b38
VafelkaTSM <telnovsm2006@gmail.com>
2026-02-20 17:56:03 +0500

    Update MathOper.go

--- MathOper.go
+++ MathOper.go
@@ -1,5 +1,8 @@
+// This library implements mathematical operations
+// The best website about mathematics https://www.mathsisfun.com/numbers/addition.html
 package MathOper
 
+// A function that adds two int numbers and outputs their result as an int
 func Add(a int, b int) int {
 	return a + b
 }
